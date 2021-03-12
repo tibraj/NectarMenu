3541c33943bb36ec9e48a7c42e18b3fcf279ccb9
tibraj <tibraj94@gmail.com>
2021-03-12 16:16:57 -0500

    top button

---
+++ app.js
@@ -0,0 +1,17 @@
+mybutton = document.getElementById("btn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function backToTop() {
+  document.documentElement.scrollTop = 0; 
+}
